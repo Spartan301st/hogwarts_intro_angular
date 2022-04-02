b8cc37d8fae731f9455d4b8a6efcadac679af1b5
anil <anilgajiev@gmail.com>
2022-04-02 12:53:28 +0400

    attempt to solve broken images src problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,10 +31,10 @@ export class AppComponent {
     'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.background-pictures.com%2Fwp-content%2Fuploads%2F2021%2F08%2Fhogwarts-castle-background-001-1479x1536.jpg&f=1&nofb=1';
 
   images = [
-    '../assets/Gryffindor.png',
-    '../assets/Hufflepuff.png',
-    '../assets/Ravenclaw.png',
-    '../assets/Slytherin.png',
+    '/assets/Gryffindor.png',
+    '/assets/Hufflepuff.png',
+    '/assets/Ravenclaw.png',
+    '/assets/Slytherin.png',
   ];
 
   currentDate = new Date();
